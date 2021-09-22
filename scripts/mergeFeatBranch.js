5f6c8d838474fa49faab2da6f376696052912d6c
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-09-22 11:42:54 +0800

    small fix

--- scripts/mergeFeatBranch.js
+++ scripts/mergeFeatBranch.js
@@ -8,7 +8,7 @@ async function exec(command) {
       console.log(stdout);
       if (stderr || err) {
         console.log(err, stderr);
-        throw new Error(err);
+        reject();
       }
       resolve();
     });
@@ -29,26 +29,24 @@ async function readLineSync(hint) {
 }
 
 (async function () {
+  const name = await readLineSync(`Enter name of branch to be merged to DEV: `);
+
   try {
-    const name = await readLineSync(
-      `Enter name of branch to be merged to DEV: `
-    );
-
     console.log(`\n### commit ${name} ###\n`);
-    await exec("git add .").catch();
-    await exec('git commit -m "merge to dev"').catch();
-
-    console.log("\n### switch to DEV ###\n");
-    await exec("git switch dev");
-
-    console.log(`\n### merge ${name} to DEV ###\n`);
-    await exec(`git merge ${name} --no-ff`);
-
-    console.log(`\n### delete branch: ${name} ###\n`);
-    await exec(`git branch -D ${name}`);
-
-    console.log("\n\n# # # # # # # # # # # #");
-    console.log("#       ALL DONE      #");
-    console.log("# # # # # # # # # # # #");
+    await exec("git add .");
+    await exec('git commit -m "merge to dev"');
   } catch {}
+
+  console.log("\n### switch to DEV ###\n");
+  await exec("git switch dev");
+
+  console.log(`\n### merge ${name} to DEV ###\n`);
+  await exec(`git merge ${name} --no-ff`);
+
+  console.log(`\n### delete branch: ${name} ###\n`);
+  await exec(`git branch -D ${name}`);
+
+  console.log("\n\n# # # # # # # # # # # #");
+  console.log("#       ALL DONE      #");
+  console.log("# # # # # # # # # # # #");
 })();
