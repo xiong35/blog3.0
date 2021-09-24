f5980aafcd95b10983d1659cbf545d3bebc1f81f
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-09-24 18:05:39 +0800

    build fe & be

--- fe/dist/assets/Home.b1553d97.js
+++
@@ -1,1 +0,0 @@
-import{i as e,j as o}from"./index.0ba24a74.js";import{d as a,c as s,e as t}from"./vendor.dcc053a3.js";var n=a({name:"Home",setup:a=>()=>s("div",{class:"p-home"},[s("h2",null,[t("Home")]),s("button",{onMouseenter:()=>e.value=!0,onMouseleave:()=>e.value=!1,onClick:e=>o(e,{name:"posts"})},[t("to posts")])])});export{n as default};
