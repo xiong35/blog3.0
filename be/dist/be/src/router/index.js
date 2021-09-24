5446d37d01b0cdb09a282f04d8830ecfc7c94b3d
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-09-24 17:13:57 +0800

    build fe & be

--- be/dist/be/src/router/index.js
+++ be/dist/be/src/router/index.js
@@ -1,13 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Router = require("koa-router");
-const t_1 = require("../handlers/t");
 const useAuth_1 = require("../middleware/useAuth");
 const post_1 = require("./post");
 const tags_1 = require("./tags");
 const router = new Router();
 router
-    .all("/test", t_1.t)
     .use("/post", post_1.postRouter.routes())
     .use("/tag", tags_1.tagRouter.routes())
     .get("/login", (0, useAuth_1.useAuth)(false));
