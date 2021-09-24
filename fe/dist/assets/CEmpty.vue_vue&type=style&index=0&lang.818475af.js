8b9b2d834ee8f2237bf75d4cce0157e0184acbeb
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-09-24 16:06:41 +0800

    build fe

---
+++ fe/dist/assets/CEmpty.vue_vue&type=style&index=0&lang.818475af.js
@@ -0,0 +1,1 @@
+import{d as s,c as a,e as c}from"./vendor.dcc053a3.js";var e=s({name:"CEmpty",setup:s=>()=>a("div",{class:"c-empty"},[a("span",{class:"c-empty_hint"},[c("什么都没有，该有什么呢")]),a("div",{class:"c-empty_decorate"},null)])});export{e as _};
