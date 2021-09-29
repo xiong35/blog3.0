cbcaf374ff9b2133be8ae1c0e377fbfe950bb879
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-09-29 11:35:43 +0800

    build fe & be

--- be/dist/be/src/handlers/setu.js
+++ be/dist/be/src/handlers/setu.js
@@ -13,9 +13,7 @@ exports.setu = void 0;
 const UA = require("ua-device");
 const setu = (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b;
-    const { host, "user-agent": userAgent } = ctx.request.header;
-    console.log(host, userAgent);
-    console.log(UA);
+    const { "user-agent": userAgent } = ctx.request.header;
     const info = new UA(userAgent);
     const type2Name = {
         mobile: "手机",
@@ -26,11 +24,11 @@ const setu = (ctx) => __awaiter(void 0, void 0, void 0, function* () {
 想看色图??
 我是正经人, 哪来的色图!
 你是坏人.$.$.$
-小本本记下来了: 
+小本本记下来了:
 ${((_a = info === null || info === void 0 ? void 0 : info.device) === null || _a === void 0 ? void 0 : _a.type) ? "用的是" + type2Name[info.device.type] + "是吧" : "<"}
 ${((_b = info === null || info === void 0 ? void 0 : info.os) === null || _b === void 0 ? void 0 : _b.name) ? "用的是 " + info.os.name + " 系统是吧" : "<"}
 ${info.browser.name ? "用的是 " + info.browser.name + " 浏览器是吧" : "<"}
-ip 是 ${host} 是吧
+ip 是 ${ctx.request.ip} 是吧
 这就去 gank 你!`;
     ctx.response.redirect("http://www.xiong35.cn/blog3.0/?m=" + rtString);
 });
