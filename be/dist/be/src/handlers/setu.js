b43a74f61664a56d3bc56618bd1d7f441687e65a
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-09-30 19:27:36 +0800

    build fe & be

--- be/dist/be/src/handlers/setu.js
+++ be/dist/be/src/handlers/setu.js
@@ -15,21 +15,22 @@ const setu = (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b;
     const { "user-agent": userAgent } = ctx.request.header;
     const info = new UA(userAgent);
+    const { ip } = ctx.request;
     const type2Name = {
         mobile: "手机",
         tablet: "平板",
         desktop: "电脑",
     };
     let rtString = `
-想看色图??
-我是正经人, 哪来的色图!
+想看色图??$$
+我是正经人, 哪来的色图!$$$
 你是坏人.$.$.$
-小本本记下来了:
-${((_a = info === null || info === void 0 ? void 0 : info.device) === null || _a === void 0 ? void 0 : _a.type) ? "用的是" + type2Name[info.device.type] + "是吧" : "<"}
-${((_b = info === null || info === void 0 ? void 0 : info.os) === null || _b === void 0 ? void 0 : _b.name) ? "用的是 " + info.os.name + " 系统是吧" : "<"}
-${info.browser.name ? "用的是 " + info.browser.name + " 浏览器是吧" : "<"}
-ip 是 ${ctx.request.ip} 是吧
+小本本记下来了:$$$
+${((_a = info === null || info === void 0 ? void 0 : info.device) === null || _a === void 0 ? void 0 : _a.type) ? "拿着" + type2Name[info.device.type] + "是吧$$$" : "<"}
+${((_b = info === null || info === void 0 ? void 0 : info.os) === null || _b === void 0 ? void 0 : _b.name) ? "用着 " + info.os.name + " 系统是吧$$$" : "<"}
+${info.browser.name ? "打开的 " + info.browser.name + " 浏览器是吧$$$" : "<"}
+ip 是 ${ip} 是吧$$$
 这就去 gank 你!`;
-    ctx.response.redirect("http://www.xiong35.cn/blog3.0/?m=" + rtString);
+    ctx.response.redirect("http://www.xiong35.cn/blog3.0/setu?m=" + rtString);
 });
 exports.setu = setu;
