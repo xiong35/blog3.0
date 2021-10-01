cd81e2453e288b72d9dae7e8202a35203a95aa7b
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-10-01 16:12:46 +0800

    change loading effect

--- fe/src/components/CError.vue
+++ fe/src/components/CError.vue
@@ -1,15 +1,21 @@
 <script lang="tsx">
   import { defineComponent, ref } from "vue";
+  import CPlaceholder from "./CPlaceholder.vue";
 
   export default defineComponent({
     name: "CError",
     setup(props) {
-      return () => <div class="c-error">error</div>;
+      return () => (
+        <div class="c-error">
+          <CPlaceholder msg="出错了呢..." />
+        </div>
+      );
     },
   });
 </script>
 
 <style lang="scss">
   .c-error {
+    padding: 20vh 0 0;
   }
 </style>
